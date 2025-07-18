c3533e846a309a33fbed676c10458fb8c807be7d
unknown <rjg.villarroel@yavirac.edu.ec>
2025-07-18 13:18:14 -0500

    Primer

--- src/keys.js
+++ src/keys.js
@@ -1,10 +1,10 @@
-const MYSQLHOST = 'localhost'; 
-const MYSQLUSER = 'root';
-const MYSQLPASSWORD = '12345';
+const MYSQLHOST = '31.97.42.126'; 
+const MYSQLUSER = 'linkear';
+const MYSQLPASSWORD = '0987021692@Rj';
 const MYSQLDATABASE = 'petPoket';
 const MYSQLPORT = '3306';
 const MYSQL_URI = process.env.MYSQL_URI || '';
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/petPoket';
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://leonardoedi1979:leonardo2411@edisoncloud.ux4si.mongodb.net/petpoket';
 // Exportar las variables de configuración
 module.exports = {
     MYSQLHOST,
