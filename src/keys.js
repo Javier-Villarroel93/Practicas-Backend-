b7b972c69cc989be158f8adfab85eb57cea89bb9
unknown <rjg.villarroel@yavirac.edu.ec>
2025-07-18 13:25:52 -0500

    Subiendo todos los últimos cambios

--- src/keys.js
+++ src/keys.js
@@ -1,7 +1,7 @@
 const MYSQLHOST = '31.97.42.126'; 
 const MYSQLUSER = 'linkear';
 const MYSQLPASSWORD = '0987021692@Rj';
-const MYSQLDATABASE = 'petPoket';
+const MYSQLDATABASE = 'Petpoket';
 const MYSQLPORT = '3306';
 const MYSQL_URI = process.env.MYSQL_URI || '';
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://leonardoedi1979:leonardo2411@edisoncloud.ux4si.mongodb.net/petpoket';
